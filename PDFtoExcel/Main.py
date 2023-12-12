af017576c03986fcbbc97ef88c3d2823ffb5eee4
Mistborntt <95579383+Mistborntt@users.noreply.github.com>
2023-12-12 15:09:20 +0800

    Update Main.py

--- PDFtoExcel/Main.py
+++ PDFtoExcel/Main.py
@@ -1,6 +1,10 @@
 from TransferNotice import extract_data_notice
 from TransferResult import extract_data_result
 
+# 将当前工作目录设置为脚本所在的目录
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
 pdf_path1 = r'./PDF样例/转让公告1.pdf'
 excel_path1 = r'/Users/tt/Desktop/转让公告1.xlsx'
 
